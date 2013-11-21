b60fcd96320dbc4701c1cc8bdf2a69cb70d7954d
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2013-11-21 19:20:58 +0100

    copy-paste remnants removed

--- test/test_platform_operations.rb
+++ test/test_platform_operations.rb
@@ -3,14 +3,6 @@ require "minitest/autorun"
 require "mocha/setup"
 require "gaudi"
 
-module TestHelpers
-  def mock_configuration lines
-    File.expects(:exists?).returns(true)
-    File.expects(:readlines).returns(lines)
-    File.join(File.dirname(__FILE__),'system.cfg')
-  end
-end
-
 class TestPlatformOperations < MiniTest::Unit::TestCase
   include PlatformOperations
   def test_extensions
