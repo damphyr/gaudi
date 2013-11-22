8345cd4a0ed748fb013587b87a29a9fab9810406
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2013-11-22 15:29:30 +0100

    switch_configuration helper

--- lib/gaudi/helpers/utilities.rb
+++ lib/gaudi/helpers/utilities.rb
@@ -12,3 +12,26 @@ def mass_require filelist
     end
   end
 end
+
+#Switches the configuration for the given block
+def switch_configuration configuration_file
+  if block_given?
+    current_configuration =  ENV['GAUDI_CONFIG']
+    if File.expand_path(configuration_file) != File.expand_path(current_configuration)
+      begin
+        puts "Switching configuration to #{configuration_file}"
+        $configuration=nil
+        ENV['GAUDI_CONFIG'] = configuration_file
+        $configuration=Gaudi::Configuration::SystemConfiguration.load([ENV['GAUDI_CONFIG']])
+        yield
+      ensure
+        puts "Switching configuration back to #{current_configuration}"
+        $configuration=nil
+        ENV['GAUDI_CONFIG'] = current_configuration
+        $configuration=Gaudi::Configuration::SystemConfiguration.load([ENV['GAUDI_CONFIG']])
+      end
+    else
+      yield
+    end
+  end
+end
