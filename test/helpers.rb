d31a29a4dace87ca9bb47a71b57f38d48cee0452
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2013-11-26 14:46:28 +0100

    some filesystems are case sensitive
    imagine that!

--- test/helpers.rb
+++ test/helpers.rb
@@ -7,21 +7,21 @@ module TestHelpers
   end
   def directory_fixture
     base=File.dirname(__FILE__)
-    mkdir_p("#{base}/tmp/common/Foo/inc",:verbose=>false)
-    mkdir_p("#{base}/tmp/mingw/Foo/test",:verbose=>false)
-    touch("#{base}/tmp/mingw/Foo/foo.c",:verbose=>false)
-    touch("#{base}/tmp/mingw/Foo/foop.cpp",:verbose=>false)
-    touch("#{base}/tmp/mingw/Foo/bar.asm",:verbose=>false)
-    touch("#{base}/tmp/mingw/Foo/test/f.c",:verbose=>false)
-    touch("#{base}/tmp/mingw/Foo/test/f.cpp",:verbose=>false)
-    touch("#{base}/tmp/mingw/Foo/foo.h",:verbose=>false)
-    touch("#{base}/tmp/common/Foo/pub.c",:verbose=>false)
-    touch("#{base}/tmp/common/Foo/pub.cpp",:verbose=>false)
-    touch("#{base}/tmp/common/Foo/inc/pub.h",:verbose=>false)
-    mkdir_p("#{base}/tmp/common/Bar",:verbose=>false)
-    touch("#{base}/tmp/common/Bar/bar.c",:verbose=>false)    
-    File.open("#{base}/tmp/common/Bar/build.cfg","wb"){|f| f.write(['prefix=BAR'].join("\n"))}
-    File.open("#{base}/tmp/common/Foo/build.cfg","wb"){|f| f.write(['prefix=FOO','deps=BAR','incs= ./inc','libs= foo.lib,bar.lib'].join("\n"))}
+    mkdir_p("#{base}/tmp/common/FOO/inc",:verbose=>false)
+    mkdir_p("#{base}/tmp/mingw/FOO/test",:verbose=>false)
+    touch("#{base}/tmp/mingw/FOO/foo.c",:verbose=>false)
+    touch("#{base}/tmp/mingw/FOO/foop.cpp",:verbose=>false)
+    touch("#{base}/tmp/mingw/FOO/bar.asm",:verbose=>false)
+    touch("#{base}/tmp/mingw/FOO/test/f.c",:verbose=>false)
+    touch("#{base}/tmp/mingw/FOO/test/f.cpp",:verbose=>false)
+    touch("#{base}/tmp/mingw/FOO/foo.h",:verbose=>false)
+    touch("#{base}/tmp/common/FOO/pub.c",:verbose=>false)
+    touch("#{base}/tmp/common/FOO/pub.cpp",:verbose=>false)
+    touch("#{base}/tmp/common/FOO/inc/pub.h",:verbose=>false)
+    mkdir_p("#{base}/tmp/common/BAR",:verbose=>false)
+    touch("#{base}/tmp/common/BAR/bar.c",:verbose=>false)    
+    File.open("#{base}/tmp/common/BAR/build.cfg","wb"){|f| f.write(['prefix=BAR'].join("\n"))}
+    File.open("#{base}/tmp/common/FOO/build.cfg","wb"){|f| f.write(['prefix=FOO','deps=BAR','incs= ./inc','libs= foo.lib,bar.lib'].join("\n"))}
     return "#{base}/tmp"
   end
 end
