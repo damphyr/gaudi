720253d7142adb819bf82cca6d540b900835f195
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2013-12-06 16:31:23 +0100

    added switch_platform_configuration
    more rdoc for the rest

--- lib/gaudi/helpers/utilities.rb
+++ lib/gaudi/helpers/utilities.rb
@@ -14,6 +14,11 @@ def mass_require filelist
 end
 
 #Switches the configuration for the given block
+#
+#That is it switches the system configuration by reading a completely different file.
+#
+#This makes for some interesting usage when you don't want to have multiple calls
+#with a different GAUDI_CONFIG parameter
 def switch_configuration configuration_file
   if block_given?
     current_configuration =  ENV['GAUDI_CONFIG']
@@ -30,12 +35,26 @@ def switch_configuration configuration_file
         ENV['GAUDI_CONFIG'] = current_configuration
         $configuration=Gaudi::Configuration::SystemConfiguration.load([ENV['GAUDI_CONFIG']])
       end
-    else
-      yield
     end
   end
 end
 
+#Switches platform configuration replacing platform with the contents of the file
+#for the given block
+def switch_platform_configuration configuration_file,system_config,platform
+  if block_given?
+    begin
+      current_config=system_config.platform_config(platform)
+      new_cfg=system_config.read_configuration(configuration_file,[],[])
+      system_config.set_platform_config(new_cfg,platform)
+      yield
+    rescue
+      raise $!
+    ensure
+      system_config.set_platform_config(current_config,platform)
+    end
+  end
+end
 #Writes a file making sure the directory is created
 def write_file filename,content
   mkdir_p(File.dirname(filename),:verbose=>false)
