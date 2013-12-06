a507374ebe5b06a2e8b95b8a37fe2109a8dbdfc1
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2013-12-06 16:33:05 +0100

    graph:deployment task (needs graph gem)

---
+++ lib/gaudi/tasks/doc.rb
@@ -0,0 +1,27 @@
+def graph_deployment(deployment)
+  deployment.platforms.each do |platform| 
+    cluster platform do
+      label platform
+      deployment.programs(platform).each do |program|
+        cluster program.name do
+          label program.name
+          program.dependencies.each do |dependency|
+            dependency.dependencies.each{|dep| edge "#{dependency.name}",dep.name}
+          end
+        end#program cluster
+      end#each program
+    end#platform cluster
+  end#each platform
+end
+
+desc "Creates a component dependency graph for DEPLOYMENT.\n rake graph:deployment DEPLOYMENT=Foo"
+task :"graph:deployment" do
+  require 'graph'
+  deployment=Gaudi::Deployment.new($configuration.deployment,$configuration)
+  digraph do
+    boxes
+    graph_deployment(deployment)
+    mkdir_p($configuration.out_dir,'graphs',:verbose=>false)
+    save File.join($configuration.out_dir,deployment.name), "png"
+  end#graph
+end
