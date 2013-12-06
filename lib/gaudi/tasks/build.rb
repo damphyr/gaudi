78432cc41286cf46cf666e01063bbcce95269e5d
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2013-12-06 16:33:20 +0100

    standard build:deployment task

---
+++ lib/gaudi/tasks/build.rb
@@ -0,0 +1,9 @@
+namespace :build do
+  desc "Builds the deployment specified with DEPLOYMENT.\n rake build:deployment DEPLOYMENT=Foo"
+  task :deployment do
+    include Gaudi::Tasks
+    deployment=Gaudi::Deployment.new($configuration.deployment,$configuration)
+    t=deployment_task(deployment,$configuration)
+    Rake::Task[t].invoke
+  end
+end
