571daaa7fa370d80029ba2f2f62967798b77f60a
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2013-12-13 12:56:18 +0100

    minor doc fix

--- lib/gaudi/helpers/errors.rb
+++ lib/gaudi/helpers/errors.rb
@@ -3,6 +3,6 @@
 #If at any point there is a different exception type in the code then it's going to be a bug
 class GaudiError< RuntimeError
 end
-#Configuration error
+#Raised whenever an error is encountered while handling configuration files
 class GaudiConfigurationError< GaudiError
 end
