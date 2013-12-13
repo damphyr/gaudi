9d8b8feff92ed7a34edcc101950a86cbd445c844
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2013-12-13 12:55:52 +0100

    don't mpolute the global namespace

--- lib/gaudi.rb
+++ lib/gaudi.rb
@@ -8,24 +8,26 @@ include Gaudi::Utilities
 #load every file you find in the helpers directory
 mass_require(Rake::FileList["#{File.join(File.dirname(__FILE__),'gaudi/helpers')}/*.rb"].exclude("utilities.rb"))
 
-#Reads the configuration and sets the environment up
-#
-#This is the system's entry point
-def env_setup work_dir
-  #Ensure that stdout and stderr output is properly ordered
-  $stdout.sync=true
-  $stderr.sync=true
-  #Tell rake not to truncate it's task output
-  Rake.application.terminal_columns = 999
-  unless $configuration
-    #we're looking for the configuration
-    if ENV['GAUDI_CONFIG']
-      system_config=Gaudi::Configuration::SystemConfiguration.load([ENV['GAUDI_CONFIG']])
-      system_config.workspace=File.expand_path(work_dir)
-      $configuration=system_config
-    else
-      if !ARGV.empty? && ARGV.include?('T')
-        raise GaudiError,"Did not specify a configuration.\n Add GAUDI_CONFIG=path/to/config to the commandline or specify the GAUDI_CONFIG environment variable"
+module Gaudi::Utilities
+  #Reads the configuration and sets the environment up
+  #
+  #This is the system's entry point
+  def env_setup work_dir
+    #Ensure that stdout and stderr output is properly ordered
+    $stdout.sync=true
+    $stderr.sync=true
+    #Tell rake not to truncate it's task output
+    Rake.application.terminal_columns = 999
+    unless $configuration
+      #we're looking for the configuration
+      if ENV['GAUDI_CONFIG']
+        system_config=Gaudi::Configuration::SystemConfiguration.load([ENV['GAUDI_CONFIG']])
+        system_config.workspace=File.expand_path(work_dir)
+        $configuration=system_config
+      else
+        if !ARGV.empty? && ARGV.include?('T')
+          raise GaudiError,"Did not specify a configuration.\n Add GAUDI_CONFIG=path/to/config to the commandline or specify the GAUDI_CONFIG environment variable"
+        end
       end
     end
   end
