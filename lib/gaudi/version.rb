c9b9a8f2031061d5785f5b3ad72f45a6541ff789
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2014-05-05 10:17:50 +0200

    Bumped the version
    (have achieved semi stable structure)

--- lib/gaudi/version.rb
+++ lib/gaudi/version.rb
@@ -1,12 +1,12 @@
 module Gaudi
-  #Gaudi follows SemVer evn though it's not a gem
+  #Gaudi follows SemVer even though it's not a gem
   module Version
     #Major version
     MAJOR=0
     #Minor version
-    MINOR=0
+    MINOR=1
     #Tiny version
-    TINY=1
+    TINY=0
     #All-in-one
     STRING=[MAJOR,MINOR,TINY].join('.')
   end
