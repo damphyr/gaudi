582fb75561ef06abfb32f72c6c5292989031e29c
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2020-05-28 23:37:16 +0200

    Ran the code through the autoformater and linted it

--- gem/lib/gaudi/version.rb
+++ gem/lib/gaudi/version.rb
@@ -1,15 +1,15 @@
 module Gaudi
-  #Gaudi follows SemVer and so does it's gem, but they're two separate things
+  # Gaudi follows SemVer and so does it's gem, but they're two separate things
   class Gem
     module Version
-      #Major version
-      MAJOR=0
-      #Minor version
-      MINOR=5
-      #Tiny version
-      TINY=1
-      #All-in-one
-      STRING=[MAJOR,MINOR,TINY].join('.')
+      # Major version
+      MAJOR = 0
+      # Minor version
+      MINOR = 5
+      # Tiny version
+      TINY = 1
+      # All-in-one
+      STRING = [MAJOR, MINOR, TINY].join(".")
     end
   end
 end
