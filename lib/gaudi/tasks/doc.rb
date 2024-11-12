880a05dcc79e85d48bd7147f398ad965a521aad8
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2024-11-12 23:33:19 +0100

    the last Windows remnant

--- lib/gaudi/tasks/doc.rb
+++ lib/gaudi/tasks/doc.rb
@@ -2,7 +2,7 @@ require 'rdoc'
 
 RDoc::Task.new(:rdoc=>"doc:gaudi", :clobber_rdoc => "clean:doc:gaudi",:rerdoc=>"doc:gaudi:force") do |rdoc|
   rdoc.title= "Build System"
-  rdoc.rdoc_files.include("#{$configuration.base}/doc/BUILDSYSTEM.md", "#{$configuration.base}/Tools/build/lib/**/*.rb")
+  rdoc.rdoc_files.include("#{$configuration.base}/doc/BUILDSYSTEM.md", "#{$configuration.base}/tools/build/lib/**/*.rb")
   rdoc.rdoc_dir=File.join($configuration.out,"doc/gaudi")
   rdoc.main="BUILDSYSTEM.md"
   rdoc.options+=["--page-dir=#{$configuration.base}/doc","-O"]
